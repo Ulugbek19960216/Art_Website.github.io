52df754f860ed24dbfd334bd4a77e8e6f42a8b6b
Ulugbek_Leo <sherovulugbek04@gmail.com>
2023-07-28 03:14:32 +0500

    commented code

--- category.js
+++ category.js
@@ -1,37 +1,38 @@
-var swiper = new Swiper(".slide-container", {
-    slidesPerView: 4,
-    autoplay: {
-      delay: 2000,
-    },
-    spaceBetween: 20,
-    sliderPerGroup: 4,
-    loop: true,
-    centerSlide: "true",
-    fade: "true",
-    grabCursor: "true",
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-      0: {
-        slidesPerView: 1,
-      },
-      520: {
-        slidesPerView: 2,
-      },
-      768: {
-        slidesPerView: 3,
-      },
-      1000: {
-        slidesPerView: 4,
-      },
-    },
-  });
 
-  
+var swiper = new Swiper(".slide-container", {
+  slidesPerView: 4, // Number of slides to show in the slider at once
+  autoplay: {
+    delay: 2000, // Delay between slide transitions in autoplay mode (in milliseconds)
+  },
+  spaceBetween: 20, // Space (in pixels) between each slide
+  sliderPerGroup: 4, // Number of slides to group together when using slidesPerView: 4
+  loop: true, // Enable continuous loop mode for the slider
+  centerSlide: "true", // Center the currently active slide
+  fade: "true", // Enable fade transition effect between slides
+  grabCursor: "true", // Show a grab cursor when hovering over the slider
+  pagination: {
+    el: ".swiper-pagination", // Specifying the element to display pagination bullets
+    clickable: true, // Enabling the pagination bullets to be clickable
+    dynamicBullets: true, // Dynamically generate pagination bullets based on the number of slides
+  },
+  navigation: {
+    nextEl: ".swiper-button-next", // Specifying the element for the next slide button
+    prevEl: ".swiper-button-prev", // Specifying the element for the previous slide button
+  },
+  breakpoints: {
+    // Responsive breakpoints for different screen sizes
+    0: {
+      slidesPerView: 1, // Number of slides to show at 0px and above (e.g., mobile devices)
+    },
+    520: {
+      slidesPerView: 2, // Number of slides to show at 520px and above (e.g., small tablets)
+    },
+    768: {
+      slidesPerView: 3, // Number of slides to show at 768px and above (e.g., tablets)
+    },
+    1000: {
+      slidesPerView: 4, // Number of slides to show at 1000px and above (e.g., desktops)
+    },
+  },
+});
+
--- index.js
+++ index.js
@@ -1,43 +1,53 @@
-const toggleBtn = document.querySelector('.toggle-btn')
-const toggleBtnIcon = document.querySelector('.toggle-btn i')
-const dropDownMenu = document.querySelector('.dropdown-menu')
+// --- Dropdown Menu Toggling ---
+// Selecting the elements from the DOM
+const toggleBtn = document.querySelector('.toggle-btn'); // The toggle button element
+const toggleBtnIcon = document.querySelector('.toggle-btn i'); // The icon inside the toggle button
+const dropDownMenu = document.querySelector('.dropdown-menu'); // The dropdown menu to be shown/hidden
 
-
+// Adding a click event listener to the toggle button
 toggleBtn.onclick = function () {
-    dropDownMenu.classList.toggle('open')
-    const isOpen = dropDownMenu.classList.contains('open')
+    // Toggling the 'open' class on the dropdown menu to show or hide it
+    dropDownMenu.classList.toggle('open');
 
-    toggleBtnIcon.classList = isOpen 
-        ? 'fa-solid fa-xmark'
-        : 'fa-solid fa-bars'
-}
+    // Checking if the dropdown menu is currently open or closed
+    const isOpen = dropDownMenu.classList.contains('open');
 
+    // Changing the icon of the toggle button based on whether the dropdown is open or closed
+    toggleBtnIcon.classList = isOpen
+        ? 'fa-solid fa-xmark' // Icon when the dropdown is open
+        : 'fa-solid fa-bars'; // Icon when the dropdown is closed
+};
 
-
-// >>>>>>>> slide>>>>>>>>>>>>>>
+// --- Swiper (Slider) Initialization ---
+// Initializing a new Swiper instance for the tranding-slider element
 var TrandingSlider = new Swiper('.tranding-slider', {
-  effect: 'coverflow',
+  effect: 'coverflow', // Using the 'coverflow' effect for the slider
   autoplay: {
-    delay: 2000,
+    delay: 2000, // Setting autoplay delay to 2 seconds
   },
-  grabCursor: true,
-  centeredSlides: true,
-  loop: true,
-  slidesPerView: 'auto',
-  spaceBetween: 0,
+  grabCursor: true, // Showing a grab cursor when hovering over the slider
+  centeredSlides: true, // Centering the slides in the slider
+  loop: true, // Enabling loop to create a continuous sliding effect
+  slidesPerView: 'auto', // Automatically adjusting the number of visible slides based on container width
+  spaceBetween: 0, // Setting the space between slides to 0
   coverflowEffect: {
-    rotate: 0,
-    stretch: 0,
-    depth: 450,
-    modifier: 3,
+    rotate: 0, // Setting the rotation angle for the coverflow effect to 0
+    stretch: 0, // Setting the stretch of the coverflow effect to 0
+    depth: 450, // Setting the depth (z-index) of the slides in the coverflow effect
+    modifier: 3, // Setting the scale modifier for the slides in the coverflow effect
   },
   pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
+    el: '.swiper-pagination', // Specifying the element to display pagination bullets
+    clickable: true, // Enabling the pagination bullets to be clickable
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next', // Specifying the element for the next slide button
+    prevEl: '.swiper-button-prev', // Specifying the element for the previous slide button
   }
 });
+
+
+
+
+
 
